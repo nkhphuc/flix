8bc7e713ef60d5829d8406aa1623ef74e7f6af52
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2022-08-20 15:37:08 +0700

    m

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,4 +1,5 @@
 module MoviesHelper
+
     def total_gross(movie)
         if movie.flop?
             "Flop!"
@@ -6,15 +7,25 @@ module MoviesHelper
             number_to_currency(movie.total_gross, precision: 0)
         end
     end
+
     def year_of(movie)
         movie.released_on.year
     end
 
     def nav_link_to(text, url)
         if current_page?(url)
-          link_to(text, url, class: "active")
+            link_to(text, url, class: "active")
         else
-          link_to(text, url)
+            link_to(text, url)
         end
     end
+
+    def main_image(movie)
+        if movie.main_image.attached?
+            image_tag movie.main_image.variant(resize_to_limit: [200, nil])
+        else
+            image_tag "placeholder.png"
+        end
+    end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,6 +9,8 @@ class Movie < ApplicationRecord
   has_many :characterizations, dependent: :destroy
   has_many :genres, through: :characterizations
 
+  has_one_attached :main_image
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
@@ -37,15 +39,27 @@ class Movie < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
-  validates :image_file_name, format: {
-    with: /\w+\.(jpg|png)\z/i,
-    message: "must be a JPG or PNG image"
-  }
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :rating, inclusion: { in: RATINGS }
+  validate :acceptable_image
 
   private
+
   def set_slug
     self.slug = title.parameterize
   end
+
+  def acceptable_image
+    return unless main_image.attached?
+  
+    unless main_image.blob.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
+  
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
+  end
+
 end
